d930eacee82e0a1f26d71f11f77dd54b5c0f0ea5
Harkanwal <harkanwal@fastmail.fm>
2020-11-24 20:02:43 +1300

    Edited homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Data Notebooks NZ</h1>
+
+      <p>Journey from data to visualisation, one notebook at a time</p>
+
+    </div>
+  )
 }
